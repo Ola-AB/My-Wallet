8d075a7fd06c3134ec74cd075fb24263f8234a4c
Ola <olo2040@wp.pl>
2024-01-10 14:35:17 +0100

    added js file

---
+++ script.js
@@ -0,0 +1,54 @@
+const incomeSection = document.querySelector(".income-area");
+const expansesSection = document.querySelector(".expanses-area");
+const availableMoney = document.querySelector(".available-money");
+const addTransactionPanel = document.querySelector(".add-transaction-panel");
+
+const nameInput = document.querySelector("#name");
+const amountInput = document.querySelector("#amount");
+const categorySelect = document.querySelector("#category");
+
+const addTransactionBtn = document.querySelector(".add-transaction");
+const deleteAllBtn = document.querySelector(".delete-all");
+const deleteBtn = document.querySelector(".delete");
+const saveBtn = document.querySelector(".save");
+const cancelBtn = document.querySelector(".cancel");
+
+const lightBtn = document.querySelector(".light");
+const darkBtn = document.querySelector(".dark");
+
+let root = document.documentElement;
+let ID = 0;
+let categoryIcon;
+let selectedCategory;
+let moneyArr = [0];
+
+const showPanel = () => {
+	addTransactionPanel.style.display = "flex";
+};
+
+const closePanel = () => {
+	addTransactionPanel.style.display = "none";
+	clearInputs();
+};
+
+const checkForm = () => {
+	if (
+		nameInput.value !== "" &&
+		amountInput.value !== "" &&
+		categorySelect.value !== "none"
+	) {
+		console.log("ok");
+	} else {
+		alert("Wypełnij wszystkie pola");
+	}
+};
+
+const clearInputs = () => {
+	nameInput.value = "";
+	amountInput.value = "";
+	categorySelect.selectedIndex = 0;
+};
+
+addTransactionBtn.addEventListener("click", showPanel);
+cancelBtn.addEventListener("click", closePanel);
+saveBtn.addEventListener("click", checkForm);
